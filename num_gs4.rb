67d755c845a157f7aa269ff7dd45f8d0b667b81f
TTCAD\n-miyamoto <n-miyamoto@ttc-net.co.jp>
2018-04-26 13:03:43 +0900

    4桁数当てゲーム変更 ・initialize要素変更(listをインスタンス、num・blwをローカルに) ・正解配列生成：delete_at→.takeに変更 ・inputメソッド→inputとhit_blwに分割 ・入力した値の判断に正規表現を使用(L.19) ・eachに.with_index追加 ・注記を追加

--- num_gs4.rb
+++ num_gs4.rb
@@ -1,21 +1,58 @@
 class Number_guess
-
-  def initialize(add = [])
-   # @num = num
+  
+  def initialize(ary = [], hit = 0, cnt = 0, list = [])
     @ary = ary
-   # @add = add
+    @hit = hit
+    @cnt = cnt
+    @list = list
+    mkary
+  end
+  
+  def mkary #正解配列生成
+    ary_tmp = Range.new(0,9).to_a
+    @ary = ary_tmp.shuffle.take(4)
+  end
+  
+  def input #入力
+    num = 0
+    p "pls input 4 digit number"
+    gets.to_s.match(/^\d{4}$/) do |md| # 入力&正規表現
+      num = md[0] #numに代入
+    end
+    @list = num.chars.map(&:to_i) #入力配列化
+  end
+  
+  def hit_blw #hit&blow判断
+    blw = 0
+    @hit = 0
+    @list.each.with_index do |v,k| #listのkey,valueをそれぞれ取り出す
+      if v == @ary[k] #hit判定
+        @hit += 1
+        next
+      end
+      blw += 1 if @ary.include?(v) #blow判定
+    end
+    p "Hit = #{@hit}, Blow = #{blw}"
+    @cnt += 1
   end
 
-  def mkary
-    tary = Range.new(1,9).to_a
-　　shfl = tary.shuffle
-　　4.times do |n|
-　　  @add << shfl[n]
-　　end
-　　p @add
-　end
+  def dec
+    begin
+    loop do
+      input
+      hit_blw if @list.uniq.length == 4
+      if @hit == 4
+        p "#{@cnt}th challenge correct!!"
+        break
+      end
+    end
+    rescue => e
+      p "Error, #{e}"
+    ensure
+      p "Done."
+    end
+  end
 end
 
-
 ng = Number_guess.new
-ng.mkary
+ng.dec
